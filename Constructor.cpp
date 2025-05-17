a3b471d24c5cb366c38e92e93ff23056e0c73a3d
mansoor <t.alyamani95@gmail.com>
2025-05-17 16:31:02 +0700

    Forward declarations allow classes to be referenced before definition

---
+++ Constructor.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  
+#include <string>    
+
+using namespace std; 
+
+// Forward declarations allow classes to be referenced before definition
+class Mahasiswa;
+class Dosen;
+class Staff;
