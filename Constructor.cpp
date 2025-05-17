24bae50126008a24bbb61d72b7fd2577832e4d81
mansoor <t.alyamani95@gmail.com>
2025-05-17 16:31:41 +0700

    Universitas offers controlled access to private salary information

--- Constructor.cpp
+++ Constructor.cpp
@@ -7,3 +7,12 @@ using namespace std;
 class Mahasiswa;
 class Dosen;
 class Staff;
+
+// Universitas offers controlled access to private salary information
+class Universitas {
+public:
+    // Friend function to display a lecturer's private salary
+    static void lihatGajiDosen(Dosen* d);
+    // Friend function to display a staff member's private salary
+    static void lihatGajiStaff(Staff* s);
+};
